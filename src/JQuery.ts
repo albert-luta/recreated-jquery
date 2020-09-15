931d8c9b76efa3b54e59eb4d4d039dcbcf305e6b
Albert Luta <luta.albert100@gmail.com>
2020-09-15 11:55:52 +0300

    Implemented the basic html methods

--- src/JQuery.ts
+++ src/JQuery.ts
@@ -44,6 +44,84 @@ class JQuery {
 
 		return this;
 	}
+
+	html(): string;
+	html(input: string): this;
+	html(input: (index: number, originalText: string) => string): this;
+	html(input?: any): any {
+		if (input == null) {
+			return this.elements.reduce((acc, curr) => acc + curr.innerHTML, '');
+		}
+
+		if (typeof input === 'string') {
+			this.elements.forEach((el) => (el.innerHTML = input));
+		} else if (typeof input === 'function') {
+			this.elements.forEach((el, index) => {
+				el.innerHTML = input(index, el.innerHTML);
+			});
+		}
+
+		return this;
+	}
+
+	val(): string;
+	val(input: string): this;
+	val(input: (index: number, originalText: string) => string): this;
+	val(input?: any): any {
+		if (input == null) {
+			return this.elements.reduce((acc, curr) => {
+				if (!(curr instanceof HTMLInputElement)) return acc;
+
+				return acc + curr.value;
+			}, '');
+		}
+
+		if (typeof input === 'string') {
+			this.elements.forEach((el) => {
+				if (el instanceof HTMLInputElement) el.value = input;
+			});
+		} else if (typeof input === 'function') {
+			this.elements.forEach((el, index) => {
+				if (el instanceof HTMLInputElement) el.value = input(index, el.value);
+			});
+		}
+
+		return this;
+	}
+
+	attr(attribute: string): string | null;
+	attr(attribute: { [attribute: string]: string }): this;
+	attr(attribute: string, modifier: string): this;
+	attr(attribute: string, modifier: (index: number, originalValue: string) => string): this;
+	attr(attribute: any, modifier?: any): any {
+		if (modifier == null) {
+			if (typeof attribute === 'string') {
+				if (!this.elements.length) return null;
+
+				return this.elements[0].getAttribute(attribute);
+			} else {
+				const attributeEntries = Object.entries(attribute);
+
+				this.elements.forEach((el) => {
+					attributeEntries.forEach(([a, value]) => el.setAttribute(a, value as string));
+				});
+
+				return this;
+			}
+		} else {
+			if (typeof modifier === 'string') {
+				this.elements.forEach((el) => {
+					el.setAttribute(attribute, modifier);
+				});
+			} else {
+				this.elements.forEach((el, index) => {
+					el.setAttribute(attribute, modifier(index, el.getAttribute(attribute)));
+				});
+			}
+
+			return this;
+		}
+	}
 }
 
 export default JQuery;
